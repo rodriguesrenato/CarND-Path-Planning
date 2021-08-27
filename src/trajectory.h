90c2447b89015e4acc1d93a0fa53c6613288702d
rodriguesrenato <renato.rodrigues.rfr@gmail.com>
2021-08-27 01:44:25 -0300

    Refactor: standarize variable names and documentation

--- src/trajectory.h
+++ src/trajectory.h
@@ -1,33 +1,30 @@
 
 #ifndef TRAJECTORY_H
 #define TRAJECTORY_H
+#include <limits>
 #include <string>
 #include <vector>
-#include <limits> 
+
 #include "states.h"
 using std::string;
 using std::vector;
 
 class Trajectory {
  public:
-    Trajectory();
-    vector<double> x{};
-    vector<double> y{};
-    float cost{std::numeric_limits<float>::max() -1.0};
-    double prev_speed{0};
-    double prev_lane{0};
-    double prev_d{0};
-    double final_speed{0};
-    double final_lane{0};
-    double final_s{0};
-    double final_d{0};
-    double projected_lane{0};
-    double lane_speed{0};
-    double target_speed{0};
-    int target_lane{0};
-    vector<float> cost_debug{};
-    string debug_msg{};
-    State state{State::R};
-    State prev_state{State::R};
+  Trajectory();
+  vector<double> x{};          // Vector of x positions
+  vector<double> y{};          // Vector of y positions
+  double final_s{0};           // Final s positions
+  double final_d{0};           // Final d positions
+  double final_speed{0};       // Final speed positions
+  double final_speed_prev{0};  // Final speed positions from previous trajectory
+  int projected_lane{0};       // Projected lane
+  double projected_speed{0};   // Projected speed
+  double target_speed{0};      // Target speed for this trajectory
+  int target_lane{0};          // Target lane
+  float cost{std::numeric_limits<float>::max() - 1.0};  // Total cost
+  vector<float> costs{};                                // Vector of costs
+  State state{State::R};                                // Trajectory State
+  State prev_state{State::R};                           // Previous State
 };
 #endif
